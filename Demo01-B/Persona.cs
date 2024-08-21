b7cc421a6c4d9c034bb8de2f08efd4586af0f0be
Alumno <Alumno@LAB1507-23>
2024-08-20 19:56:19 -0500

    Agregar archivos de proyecto.

---
+++ Demo01-B/Persona.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01_B
+{
+    public class Persona
+    {
+        public Persona()
+        {
+            
+        }
+        public Persona(string nombres, string apellidos)
+        {
+            Nombres = nombres;
+            Apellidos = apellidos;
+        }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public int Edad { get; set; }
+        public string NombresCompletos { get; set; }
+
+        //Procedimiento
+        public void AsignarNombresCompletos()
+        {
+            NombresCompletos = Nombres + " " + Apellidos;
+        }
+
+        //Función
+        public string MayorEdad()
+        {
+            string mensaje = "Es menor de edad";
+            if (Edad > 18)
+                mensaje = "Es mayor de edad";
+
+            return mensaje;
+        }
+    }
+}
---
+++ Demo01-B/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01_B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.Nombres = "Hugo Felipe";
+            persona.Apellidos = "Torrico Marquez";
+
+            persona.AsignarNombresCompletos();
+            Console.WriteLine(persona.NombresCompletos);
+
+            Persona persona1 = new Persona("Reyna","Torrico");
+            Persona persona2 = new Persona
+            {
+                Nombres = "Derek",
+                Apellidos = "Torrico",
+                Edad = 5
+            }
+
+
+            Triangulo triangulo = new Triangulo();
+            triangulo.Altura = 5;
+            triangulo.Base = 4;
+
+
+            Console.WriteLine(triangulo.CalcularArea());
+            Console.WriteLine("Hola mundo");
+            Console.Read();
+
+        }
+    }
+}
